bdba779689ac1ee9149f8526b0c84b1875efd799
Liam MacPherson <liammacpherson1996@gmail.com>
2024-03-14 14:02:27 +0000

    Initial commit

---
+++ __tests__/main.test.ts
@@ -0,0 +1,42 @@
+/**
+ * Unit tests for the action's main functionality, src/main.ts
+ *
+ * These should be run as if the action was called from a workflow.
+ * Specifically, the inputs listed in `action.yml` should be set as environment
+ * variables following the pattern `INPUT_<INPUT_NAME>`.
+ */
+
+/* eslint  @typescript-eslint/no-unused-vars: "off" */ // Ignore until implementation.
+
+import * as core from '@actions/core'
+import * as main from '../src/main'
+
+// Mock the action's main function
+const runMock = jest.spyOn(main, 'run')
+
+// Other utilities
+const timeRegex = /^\d{2}:\d{2}:\d{2}/
+
+// Mock the GitHub Actions core library
+let debugMock: jest.SpiedFunction<typeof core.debug>
+let errorMock: jest.SpiedFunction<typeof core.error>
+let getInputMock: jest.SpiedFunction<typeof core.getInput>
+let setFailedMock: jest.SpiedFunction<typeof core.setFailed>
+let setOutputMock: jest.SpiedFunction<typeof core.setOutput>
+
+describe('action', () => {
+  beforeEach(() => {
+    jest.clearAllMocks()
+
+    debugMock = jest.spyOn(core, 'debug').mockImplementation()
+    errorMock = jest.spyOn(core, 'error').mockImplementation()
+    getInputMock = jest.spyOn(core, 'getInput').mockImplementation()
+    setFailedMock = jest.spyOn(core, 'setFailed').mockImplementation()
+    setOutputMock = jest.spyOn(core, 'setOutput').mockImplementation()
+  })
+
+  // Add dummy test to pass CI - this should be replaced during implementation.
+  it('should pass', async () => {
+    expect(true).toBe(true)
+  })
+})
