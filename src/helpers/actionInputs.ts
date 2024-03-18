cec6f1b9a0aebd92b02c4bbc6b3900ef7e546b2c
Liam MacPherson <11508628+LiamMacP@users.noreply.github.com>
2024-03-18 11:13:26 +0000

    feat: add initial action inputs (#3)
    Add the initial presumed inputs for the GitHub Action. These are backed by the API request currently documented. Loose validation has been conducted to proactively provide feedback upon usage.

---
+++ src/helpers/actionInputs.ts
@@ -0,0 +1,80 @@
+import * as core from '@actions/core'
+
+/**
+ * ActionInputs is a factory function that returns an object with the action inputs
+ * @returns {ActionInputs} The action inputs
+ */
+
+export interface ActionInputResult {
+  token: string
+  state: string
+  context: string
+  description: string
+  sha: string
+  owner: string
+  repo: string
+}
+
+export const ActionInputs = (): ActionInputResult => {
+  type InputValidation = (name: string, input: string) => boolean
+
+  const hasValidState: InputValidation = (
+    name: string,
+    input: string
+  ): boolean => {
+    const valid = ['success', 'failure', 'error', 'pending'].includes(input)
+
+    if (!valid) {
+      throw new Error(
+        `Invalid value for input '${name}', must be one of 'success', 'failure', 'error', 'pending'.`
+      )
+    }
+
+    return true
+  }
+
+  const hasValidSha = (name: string, input: string): boolean => {
+    if (!/^[a-f0-9]{40}$/i.test(input)) {
+      throw new Error(
+        `Invalid value for input '${name}', must be a valid SHA-1 hash.`
+      )
+    }
+
+    return true
+  }
+
+  const hasValue: InputValidation = (name: string, input: string): boolean => {
+    if (isUndefinedOrEmpty(input)) {
+      throw new Error(`Input '${name}' is required.`)
+    }
+
+    return true
+  }
+
+  const parseInput = (
+    name: string,
+    required: boolean,
+    validate: InputValidation
+  ): string => {
+    const value = core.getInput(name, { required })
+
+    if (!validate(name, value)) {
+      throw new Error(`Invalid value for input '${name}'.`)
+    }
+
+    return value
+  }
+
+  const isUndefinedOrEmpty = (input: string): boolean =>
+    input === undefined || input === ''
+
+  return {
+    token: parseInput('token', false, hasValue),
+    state: parseInput('state', true, hasValidState),
+    context: parseInput('context', true, hasValue),
+    description: parseInput('description', false, () => true),
+    sha: parseInput('sha', false, hasValidSha),
+    owner: parseInput('owner', false, hasValue),
+    repo: parseInput('repo', false, hasValue)
+  }
+}
